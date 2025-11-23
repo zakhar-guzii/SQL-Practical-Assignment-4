89cfe08d7039312865bbb019d7f754d71d13cf76
Zakhar Guzii <zakharguzii@gmail.com>
2025-11-23 23:13:55 +0200

    end analytics.sql

--- SQL/analytics.sql
+++ SQL/analytics.sql
@@ -1,5 +1,3 @@
-
-
 --1
 select reviews_summary.appid,
        name_from_applist,
@@ -19,13 +17,14 @@ order by release_year;
 
 --3
 select game_genres.genre_description,
-       count(game_genres.appid) as games_count,
+       count(game_genres.appid)                                 as games_count,
        count(case when games.price_currency = 'USD' then 1 end) as games_with_usd_price,
-       cast(avg(case when games.price_currency = 'USD' 
-                     then games.price_final_cents/100 
-                     end) as decimal) as avg_price_usd
+       cast(avg(case
+                    when games.price_currency = 'USD'
+                        then games.price_final_cents / 100
+           end) as decimal)                                     as avg_price_usd
 from game_genres
-join games on game_genres.appid = games.appid
+         join games on game_genres.appid = games.appid
 group by game_genres.genre_id, game_genres.genre_description
 order by games_count desc;
 
@@ -33,9 +32,24 @@ order by games_count desc;
 --4
 select gg.genre_description,
        round(sum(rd.author_playtime_forever) / 60.0, 2) as total_playtime_hours,
-       count(distinct rd.author_steamid) as unique_players,
+       count(distinct rd.author_steamid)                as unique_players,
        round(avg(rd.author_playtime_forever) / 60.0, 2) as avg_playtime_hours_per_review
 from reviews_detailed rd
-join game_genres gg on rd.appid = gg.appid
+         join game_genres gg on rd.appid = gg.appid
 group by gg.genre_description
 order by total_playtime_hours desc;
+
+
+--5
+select
+    gg.genre_description,
+    count(distinct g.appid) as games_count,
+    avg(rs.total_positive::FLOAT / nullif(rs.total_reviews, 0)) AS avg_positive_ratio
+from game_genres gg
+join games g on gg.appid = g.appid
+join reviews_summary rs on g.appid = rs.appid
+where rs.total_reviews > 0
+group by gg.genre_description
+having count(distinct g.appid) > 100
+order by avg_positive_ratio
+LIMIT 10;
